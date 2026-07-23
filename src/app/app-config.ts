16d195f123291d5b017d86849fbb019d44953900
Paul C <pcalverley@gmail.com>
2026-07-24 07:46:25 +1000

    Removed scaffold

--- src/app/app-config.ts
+++
@@ -1,9 +0,0 @@
-export const siteConfig = {
-  name: 'DavesFunRC',
-  url: 'https://www.davesfunrc.com',
-  youtubeUrl: 'https://www.youtube.com/@DavesFunRC',
-  cults3dUrl: 'https://cults3d.com/en/users/trevbenn/3d-models',
-  koFiUrl: 'https://ko-fi.com/davesfunrc',
-  tinkercadUrl:
-    'https://www.tinkercad.com/things/iw4zzNgaSQg-davesfunrc-shared-designs?sharecode=i2wz_nIy2qr0iHzN0VqJUTFY3An9YuIsaoJSsA0D5YA',
-};
--- src/components/layout/PageLayout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ComponentChildren } from 'preact';
-import './PageLayout.css';
-
-export function PageLayout({ children }: { children: ComponentChildren }) {
-  return <main className="page-layout container">{children}</main>;
-}
