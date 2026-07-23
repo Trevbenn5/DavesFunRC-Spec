73d034f6497f09de7fcc457eaff794403093affb
Paul C <pcalverley@gmail.com>
2026-07-24 06:51:36 +1000

    Start a fresh scaffold

--- src/components/content/NotFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-import { PagePlaceholder } from './PagePlaceholder';
-
-export function NotFoundPage() {
-  return (
-    <PagePlaceholder title="Page not found">
-      We couldn't find the page you were looking for. It may have moved, or never existed.
-    </PagePlaceholder>
-  );
-}
